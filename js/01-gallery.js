33507efa9268d10a0cdfbdd7096e4f8691145ded
Uliya22 <uliya.d@ukr.net>
2022-08-13 17:49:34 +0300

    дз 7 частина 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery')
+// console.log(galleryItems);
+const galleryMarkup = createGalleryItem(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener('click', onGalleryItemClick);
 
-console.log(galleryItems);
+// console.log(createGalleryItem(galleryItems));
+
+function createGalleryItem(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+         return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>
+        `
+    }).join('');
+
+}
+
+function onGalleryItemClick(evt) {
+    if (!evt.target.classList.contains('gallery__image')) {
+    return;
+    };
+    evt.preventDefault();
+    console.log(evt.target.dataset.source);
+
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}">
+`)
+
+instance.show()
+
+    const visible = basicLightbox.visible()
+    console.log(visible);
+}
